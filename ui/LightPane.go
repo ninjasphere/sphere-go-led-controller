b90f3b9f74e10f795bf63d9b2a3fd28f3ceaaf87
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-01 12:09:02 +1000

    Fix the throttling, add proper reply handling

--- ui/LightPane.go
+++ ui/LightPane.go
@@ -17,15 +17,19 @@ import (
 	"github.com/ninjasphere/go-ninja/logger"
 )
 
+var onOffRate = &throttle{delay: time.Millisecond * 250}
+var colorRate = &throttle{delay: time.Millisecond * 500}
+
+const colorRotateSpeed = 0.0015
+
 type LightPane struct {
 	log *logger.Logger
 
 	onOffDevices []*rpc.Client
 	colorDevices []*rpc.Client
 
-	onOffState            bool
-	onOnOffStateChange    func(bool)
-	ignoringOnOffGestures bool
+	onOffState         bool
+	onOnOffStateChange func(bool)
 
 	colorState         float64
 	onColorStateChange func(float64)
@@ -34,14 +38,12 @@ type LightPane struct {
 	offImage *Image
 }
 
-const colorRotateSpeed = 0.0025
-
 func NewLightPane(offImage string, onImage string, onOnOffStateChange func(bool), onColorStateChange func(float64), mqtt *mqtt.MqttClient) *LightPane {
 
-	onOffDevices, err := getChannelClients("light", "on-off", mqtt)
+	/*onOffDevices, err := getChannelClients("light", "on-off", mqtt)
 	if err != nil {
 		log.Fatalf("Failed to get on-off devices", err)
-	}
+	}*/
 
 	colorDevices, err := getChannelClients("light", "core.batching", mqtt)
 	if err != nil {
@@ -49,7 +51,7 @@ func NewLightPane(offImage string, onImage string, onOnOffStateChange func(bool)
 	}
 
 	log := logger.GetLogger("LightPane")
-	log.Infof("Pane got %d on/off devices", len(onOffDevices))
+	//log.Infof("Pane got %d on/off devices", len(onOffDevices))
 
 	log.Infof("Pane got %d color devices", len(colorDevices))
 
@@ -59,56 +61,86 @@ func NewLightPane(offImage string, onImage string, onOnOffStateChange func(bool)
 		onOnOffStateChange: onOnOffStateChange,
 		onColorStateChange: onColorStateChange,
 		log:                log,
-		onOffDevices:       onOffDevices,
-		colorDevices:       colorDevices,
+		//onOffDevices:       onOffDevices,
+		colorDevices: colorDevices,
 	}
 }
 
 func (p *LightPane) Gesture(gesture *gestic.GestureData) {
 
-	if gesture.Coordinates.X > 0 && gesture.Coordinates.Y > 0 && gesture.Coordinates.Z > 0 {
+	//if gesture.Coordinates.X > 0 && gesture.Coordinates.Y > 0 && gesture.Coordinates.Z > 0 {
 
-		col := p.colorState + colorRotateSpeed
-		if col >= 1 {
-			col = 0
-		}
+	col := p.colorState + colorRotateSpeed
+	if col >= 1 {
+		col = 0
+	}
+	p.colorState = col
+
+	if !onOffRate.busy && colorRate.try() {
+
 		p.SetColorState(col)
-		p.log.Infof("Color wheel %f", p.colorState)
+		p.log.Infof("Color wheel %f", col)
+
+	} else {
+		//p.log.Infof("Ignoring Color wheel... Remaining time: %d\n", remaining)
 	}
 
-	if p.ignoringOnOffGestures {
-		return
-	}
+	//	}
 
 	if strings.Contains(gesture.Touch.Name(), "Tap") {
-		p.log.Infof("Tap!")
+		if onOffRate.try() {
+			p.log.Infof("Tap!")
 
-		p.ignoringOnOffGestures = true
-
-		go func() {
-			time.Sleep(time.Millisecond * 250)
-			p.ignoringOnOffGestures = false
-		}()
-
-		p.SetOnOffState(!p.onOffState)
+			p.SetOnOffState(!p.onOffState)
+		} else {
+			//p.log.Infof("Ignoring Tap... Remaining time: %d\n", remaining)
+		}
 	}
+
 }
 
 func (p *LightPane) SetOnOffState(state bool) {
 	p.onOffState = state
-	for _, device := range p.onOffDevices {
-		if state {
-			_ = device.Go("turnOn", nil, nil, nil)
-		} else {
-			_ = device.Go("turnOff", nil, nil, nil)
-		}
-	}
-	p.onOnOffStateChange(state)
+	p.SendOnOffToDevices()
+	go p.onOnOffStateChange(state)
 }
 
 func (p *LightPane) SetColorState(state float64) {
 	p.colorState = state
 
+	p.SendColorToDevices()
+	go p.onColorStateChange(state)
+}
+
+func (p *LightPane) SendOnOffToDevices() {
+
+	for _, device := range p.onOffDevices {
+
+		timeout := make(chan bool, 1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			timeout <- true
+		}()
+
+		reply := make(chan *rpc.Call, 1)
+
+		if p.onOffState {
+			_ = device.Go("turnOn", nil, nil, reply)
+		} else {
+			_ = device.Go("turnOff", nil, nil, reply)
+		}
+
+		select {
+		case call := <-reply:
+			p.log.Infof("Got Reply with error %s", call.Error)
+		case <-timeout:
+			p.log.Infof("Setbatch timed out")
+		}
+
+	}
+}
+
+func (p *LightPane) SendColorToDevices() {
 	sat := 0.6
 
 	for _, device := range p.colorDevices {
@@ -118,17 +150,31 @@ func (p *LightPane) SetColorState(state float64) {
 			Hue:        &p.colorState,
 			Saturation: &sat,
 		}
-		transition := 300
+		transition := 500
 		brightness := 1.0
+
+		timeout := make(chan bool, 1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			timeout <- true
+		}()
+
+		reply := make(chan *rpc.Call, 1)
 
 		_ = device.Go("setBatch", &devices.LightDeviceState{
 			Color:      colorState,
 			Transition: &transition,
 			Brightness: &brightness,
-		}, nil, nil)
+		}, nil, reply)
+
+		select {
+		case call := <-reply:
+			p.log.Infof("Got Reply with error %s", call.Error)
+		case <-timeout:
+			p.log.Infof("Setbatch timed out")
+		}
 
 	}
-	p.onColorStateChange(state)
 }
 
 func (p *LightPane) Render() (*image.RGBA, error) {
@@ -151,5 +197,23 @@ func (p *LightPane) Render() (*image.RGBA, error) {
 }
 
 func (p *LightPane) IsDirty() bool {
+	return true
+}
+
+type throttle struct {
+	delay time.Duration
+	busy  bool
+}
+
+func (t *throttle) try() bool {
+	if t.busy {
+		return false
+	}
+
+	t.busy = true
+	go func() {
+		time.Sleep(t.delay)
+		t.busy = false
+	}()
 	return true
 }
