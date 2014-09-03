a20bf47b20b2aff6fe33e6c141aba955d90e1384
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-03 15:38:52 +1000

    Precompute led gamma adjustment

--- main.go
+++ main.go
@@ -116,17 +116,29 @@ func main() {
 var cmdWriteBuffer byte = 1
 var cmdSwapBuffers byte = 2
 
+// From https://diarmuid.ie/blog/post/pwm-exponential-led-fading-on-arduino-or-other-platforms
+var R = (255 * math.Log10(2)) / (math.Log10(255))
+var ledAdjust = make(map[uint8]uint8)
+
+func init() {
+	for i := 0; i < 256; i++ {
+		ledAdjust[uint8(i)] = uint8(math.Pow(2, (float64(i)/R)) - 1)
+	}
+}
+
 func write(image *image.RGBA, s io.ReadWriteCloser) {
 
 	//spew.Dump("writing image", image)
 
 	var frame [768]byte
 
-	for i := 0; i < len(image.Pix); i = i + 4 {
-		//log.Println(i)
-		frame[i/4*3] = uint8(math.Min(float64(adjustLedBrightness(image.Pix[i]))*1.5, 255))
-		frame[(i/4*3)+1] = adjustLedBrightness(image.Pix[i+1])
-		frame[(i/4*3)+2] = adjustLedBrightness(image.Pix[i+2])
+	for inPos, outPos := 0, 0; inPos < len(image.Pix); inPos = inPos + 4 {
+
+		outPos = inPos / 4 * 3
+
+		frame[outPos] = ledAdjust[image.Pix[inPos]]
+		frame[outPos+1] = ledAdjust[image.Pix[inPos+1]]
+		frame[outPos+2] = ledAdjust[image.Pix[inPos+2]]
 	}
 
 	rows := split(frame[:], 16*3)
@@ -182,11 +194,4 @@ func split(a []byte, size int) [][]byte {
 	}
 
 	return out
-}
-
-// From https://diarmuid.ie/blog/post/pwm-exponential-led-fading-on-arduino-or-other-platforms
-var R = (255 * math.Log10(2)) / (math.Log10(255))
-
-func adjustLedBrightness(in uint8) byte {
-	return uint8(math.Pow(2, (float64(in)/R)) - 1)
 }
