a326411922ab656bf9b7f05c5f030b1978ea65fd
Jon Seymour <jon.seymour@gmail.com>
2014-11-10 17:01:27 +1100

    Changes to allow caller to skip service control at its option.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- firmware/v2-mega328/write_sphere.sh
+++ firmware/v2-mega328/write_sphere.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-stop sphere-leds || true
-stop devkit-status-led || true
+
+# callers can set SKIP_SERVICE_CONTROL to true to skip service control, but by default, we do it
+# if true, caller has already arranged for services to be stopped
+
+SKIP_SERVICE_CONTROL=${SKIP_SERVICE_CONTROL:-false} 
+
+$SKIP_SERVICE_CONTROL || stop sphere-leds || true
+$SKIP_SERVICE_CONTROL || stop devkit-status-led || true
+
 echo 7 > /sys/kernel/debug/omap_mux/gpmc_a0 # RST
 echo 7 > /sys/kernel/debug/omap_mux/uart0_ctsn # MOSI
 echo 3f > /sys/kernel/debug/omap_mux/uart0_rtsn # MISO
@@ -12,12 +19,12 @@ echo 113 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio113/direction
 echo 0 > /sys/class/gpio/gpio113/value
 
-/usr/bin/avrdude -p atmega328 -c ledmatrix -P ledmatrix -v -U flash:w:matrix_driver.hex -U lfuse:w:0xaf:m -U hfuse:w:0xd9:m -F -C avrduderc
+${AVR_DUDE_BIN:-/usr/bin/avrdude} -p atmega328 -c ledmatrix -P ledmatrix -v -U flash:w:matrix_driver.hex -U lfuse:w:0xaf:m -U hfuse:w:0xd9:m -F -C avrduderc
 
 echo in > /sys/class/gpio/gpio113/direction
 echo 113 > /sys/class/gpio/unexport
 
 echo 3f > /sys/kernel/debug/omap_mux/mcasp0_ahclkr # nCS
 
-start sphere-leds || true
-start devkit-status-led || true
+$SKIP_SERVICE_CONTROL || start sphere-leds || true
+$SKIP_SERVICE_CONTROL || start devkit-status-led || true
