79ae814c9cbc03c6fa204d5745d4f54fa3cb4cf0
Jon Seymour <jon.seymour@gmail.com>
2014-11-10 17:25:36 +1100

    Don't require bash; exit with a non-zero status code on avr failure.

--- firmware/v2-mega328/write_sphere.sh
+++ firmware/v2-mega328/write_sphere.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # callers can set SKIP_SERVICE_CONTROL to true to skip service control, but by default, we do it
 # if true, caller has already arranged for services to be stopped
 
-SKIP_SERVICE_CONTROL=${SKIP_SERVICE_CONTROL:-false} 
+SKIP_SERVICE_CONTROL=${SKIP_SERVICE_CONTROL:-false}
 
 $SKIP_SERVICE_CONTROL || stop sphere-leds || true
 $SKIP_SERVICE_CONTROL || stop devkit-status-led || true
@@ -20,6 +20,7 @@ echo out > /sys/class/gpio/gpio113/direction
 echo 0 > /sys/class/gpio/gpio113/value
 
 ${AVR_DUDE_BIN:-/usr/bin/avrdude} -p atmega328 -c ledmatrix -P ledmatrix -v -U flash:w:matrix_driver.hex -U lfuse:w:0xaf:m -U hfuse:w:0xd9:m -F -C avrduderc
+rc=$?
 
 echo in > /sys/class/gpio/gpio113/direction
 echo 113 > /sys/class/gpio/unexport
@@ -28,3 +29,4 @@ echo 3f > /sys/kernel/debug/omap_mux/mcasp0_ahclkr # nCS
 
 $SKIP_SERVICE_CONTROL || start sphere-leds || true
 $SKIP_SERVICE_CONTROL || start devkit-status-led || true
+exit $rc
