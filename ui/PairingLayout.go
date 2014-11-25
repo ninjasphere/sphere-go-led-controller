bda88c2afa5a68ab292a11584e83811f1ab221c4
Elliot Shepherd <elliot@ninjablocks.com>
2014-11-25 13:26:27 +1100

    Add showDrawing and draw methods to pairing layout

--- ui/PairingLayout.go
+++ ui/PairingLayout.go
@@ -4,15 +4,17 @@ import (
 	"image"
 	"image/color"
 
+	"time"
+
 	"github.com/ninjasphere/go-ninja/api"
 	"github.com/ninjasphere/go-ninja/logger"
-	"time"
 )
 
 type PairingLayout struct {
 	currentPane Pane
 	conn        *ninja.Connection
 	log         *logger.Logger
+	drawing     *image.RGBA
 }
 
 func NewPairingLayout(c *ninja.Connection) *PairingLayout {
@@ -23,6 +25,16 @@ func NewPairingLayout(c *ninja.Connection) *PairingLayout {
 		conn: c,
 	}
 	layout.ShowIcon("loading.gif")
+
+	/*go func() {
+		time.Sleep(time.Second * 1)
+		layout.ShowDrawing()
+		for {
+			time.Sleep(time.Millisecond * 2)
+			update := []uint8{uint8(rand.Intn(16)), uint8(rand.Intn(16)), uint8(rand.Intn(255)), uint8(rand.Intn(255)), uint8(rand.Intn(255))}
+			layout.Draw([][]uint8{update})
+		}
+	}()*/
 
 	return layout
 }
@@ -45,6 +57,23 @@ func (l *PairingLayout) ShowCode(text string) {
 
 func (l *PairingLayout) ShowIcon(image string) {
 	l.currentPane = NewImagePane("./images/" + image)
+}
+
+func (l *PairingLayout) ShowDrawing() {
+	l.drawing = image.NewRGBA(image.Rect(0, 0, 16, 16))
+	l.currentPane = &ImagePane{
+		image: &Image{0, []*image.RGBA{l.drawing}},
+	}
+}
+
+func (l *PairingLayout) Draw(updates [][]uint8) {
+	for _, update := range updates {
+		offset := l.drawing.PixOffset(int(update[0]), int(update[1]))
+		l.drawing.Pix[offset] = update[2]   // R
+		l.drawing.Pix[offset+1] = update[3] // G
+		l.drawing.Pix[offset+2] = update[4] // B
+		l.drawing.Pix[offset+3] = 255       // A
+	}
 }
 
 func (l *PairingLayout) Render() (*image.RGBA, error) {
