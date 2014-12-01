d9a8870b302ef69b7c0076c8db7d6b6e1c228bb6
Elliot Shepherd <elliot@ninjablocks.com>
2014-12-01 14:51:30 +1100

    Work with new Bus api

--- ui/Certification.go
+++ ui/Certification.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/ninjasphere/go-gestic"
+	"github.com/ninjasphere/go-ninja/bus"
 	"github.com/ninjasphere/go-ninja/logger"
-	"github.com/ninjasphere/org.eclipse.paho.mqtt.golang"
 	"github.com/ninjasphere/sphere-go-led-controller/fonts/O4b03b"
 )
 
@@ -22,7 +22,7 @@ type CertPane struct {
 	tag       string
 }
 
-func NewCertPane(conn *mqtt.MqttClient) *CertPane {
+func NewCertPane(conn bus.Bus) *CertPane {
 
 	log := logger.GetLogger("CertPane")
 
@@ -30,27 +30,20 @@ func NewCertPane(conn *mqtt.MqttClient) *CertPane {
 		log: log,
 	}
 
-	filter, err := mqtt.NewTopicFilter("$location/waypoints", 0)
-	if err != nil {
-		log.Fatalf("Boom, no good", err)
-	}
-
-	receipt, err := conn.StartSubscription(func(client *mqtt.MqttClient, message mqtt.Message) {
+	_, err := conn.Subscribe("$location/waypoints", func(topic string, payload []byte) {
 		var waypoints int
-		err := json.Unmarshal(message.Payload(), &waypoints)
+		err := json.Unmarshal(payload, &waypoints)
 		if err != nil {
-			log.Infof("Failed to parse incoming waypoints json %s, from %s", err, message.Payload())
+			log.Infof("Failed to parse incoming waypoints json %s, from %s", err, payload)
 		} else {
 			log.Infof("Number of waypoints: %d", waypoints)
 		}
 		pane.waypoints = waypoints
-	}, filter)
+	})
 
 	if err != nil {
 		log.HandleError(err, "Could not start subscription to waypoint topic")
 	}
-
-	<-receipt
 
 	pane.StartRssi(conn)
 
@@ -64,16 +57,11 @@ func (p *CertPane) Gesture(gesture *gestic.GestureData) {
 var rssiRegex = regexp.MustCompile(`rssi":-(\d*)`)
 var nameRegex = regexp.MustCompile(`name":"(.{3})"`)
 
-func (p *CertPane) StartRssi(conn *mqtt.MqttClient) {
+func (p *CertPane) StartRssi(conn bus.Bus) {
 
-	filter, err := mqtt.NewTopicFilter("$device/+/TEMPPATH/rssi", 0)
-	if err != nil {
-		p.log.HandleError(err, "Could not subscribe to $device/+/TEMPPATH/rssi ")
-	}
-
-	receipt, err := conn.StartSubscription(func(client *mqtt.MqttClient, message mqtt.Message) {
-		nameFind := nameRegex.FindAllStringSubmatch(string(message.Payload()), -1)
-		rssiFind := rssiRegex.FindAllStringSubmatch(string(message.Payload()), -1)
+	_, err := conn.Subscribe("$device/+/TEMPPATH/rssi", func(topic string, payload []byte) {
+		nameFind := nameRegex.FindAllStringSubmatch(string(payload), -1)
+		rssiFind := rssiRegex.FindAllStringSubmatch(string(payload), -1)
 
 		if nameFind == nil {
 			// Not a sticknfind
@@ -86,13 +74,11 @@ func (p *CertPane) StartRssi(conn *mqtt.MqttClient) {
 			p.rssi = rssi
 		}
 
-	}, filter)
+	})
 
 	if err != nil {
 		p.log.HandleError(err, "")
 	}
-
-	<-receipt
 
 }
 
