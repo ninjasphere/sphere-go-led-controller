f091d6da065552c707bf9fca580a12602393b47c
Jon Seymour <jon@ninjablocks.com>
2014-12-02 00:24:16 +1100

    Allow images directory to be configured.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- util/resolver.go
+++ util/resolver.go
@@ -1,6 +1,13 @@
 package util
 
+var imageDir = "images/"
+
 // resolves a relative image path to a fully qialified path.
 func ResolveImagePath(relativePath string) string {
-	return "images/" + relativePath
+	return imageDir + "/" + relativePath
+}
+
+// the directory used to resolve the
+func SetImageDir(newDir string) {
+	imageDir = newDir
 }
