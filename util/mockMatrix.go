921a0fabb1502e336b81306dad3372daeba47a4d
Jon Seymour <jon@ninjablocks.com>
2015-02-18 13:47:04 +1100

    Implement a mock connection that simulates the serial interface presented by a real matrix.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

---
+++ util/mockMatrix.go
@@ -0,0 +1,109 @@
+package util
+
+import (
+	"fmt"
+	"io"
+)
+
+const (
+	stateCmd   = 0 // expecting a command byte
+	stateData  = 1 // expecting exactly 768 data bytes
+	stateSwap  = 2 // expecting a read or a command byte
+	stateClose = 3 // expecting nothing
+
+	maxWrite = 768 // the max number of bytes received in the stateData state
+)
+
+type mockMatrix struct {
+	state int // the current state of the connection
+	count int // number of bytes received since entering the data state
+}
+
+// Answers a mock for the matrix that simulates a real led matrix.
+func newMockMatrix() io.ReadWriteCloser {
+	return &mockMatrix{
+		state: stateCmd,
+		count: 0,
+	}
+}
+
+// Answers 'F' when the mock matrix is in the swap state, otherwise answers empty.
+func (m *mockMatrix) Read(p []byte) (n int, err error) {
+	switch m.state {
+	case stateSwap:
+		if len(p) == 0 {
+			return 0, fmt.Errorf("insufficient capacity")
+		} else {
+			p[0] = 'F'
+			m.count = 0
+			m.state = stateData
+			return 1, nil
+		}
+	case stateClose:
+		return 0, fmt.Errorf("stream is closed")
+	default:
+		return 0, nil
+	}
+}
+
+// Adjusts the state of the mock matrix connection according to bytes received.
+//
+// Valid sequences are zero or more iterations of:
+//
+// []byte{cmdWriteBuffer}, [768]byte, []byte{cmdSwapBuffers}...
+//
+// All other sequences will result in the connection moving into a closed state.
+func (m *mockMatrix) Write(p []byte) (n int, err error) {
+	if len(p) == 0 {
+		return 0, nil
+	} else {
+	buffer:
+		for i, c := range p {
+			switch m.state {
+			case stateCmd:
+				switch c {
+				case cmdWriteBuffer:
+					m.count = 0
+					m.state = stateData
+				case cmdSwapBuffers:
+					m.state = stateSwap
+				default:
+					m.state = stateClose
+					return i + 1, fmt.Errorf("unexpected command received while waiting for command: 0x%02x", c)
+				}
+			case stateData:
+				if i == 0 && m.count+len(p) <= maxWrite {
+					// optimization for common case where the write length is less than or equal to the full 768 bytes
+					m.count += len(p)
+					if m.count == maxWrite {
+						m.state = stateSwap
+					}
+					break buffer
+				}
+				m.count++
+				if m.count == maxWrite {
+					m.state = stateSwap
+				}
+			case stateSwap:
+				switch c {
+				case cmdWriteBuffer:
+					m.state = stateData
+					m.count = 0
+				default:
+					m.state = stateClose
+					return i + 1, fmt.Errorf("unexpected byte received while in swap state.")
+				}
+			case stateClose:
+				return 0, fmt.Errorf("stream is closed")
+			}
+		}
+		return len(p), nil
+	}
+
+}
+
+// Moves the connection into the closed state.
+func (m *mockMatrix) Close() error {
+	m.state = stateClose
+	return nil
+}
